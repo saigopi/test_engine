a2f9cce1f48f454a2d9d0d61f188e3807246967a
vaishnavij <vaishnavi.j3@gmail.com>
2017-08-10 12:33:33 +0530

    [vaishnavi] First commit - initial Setup

---
+++ src/main/java/engine/Application.java
@@ -0,0 +1,13 @@
+package engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+    }
+}
